85368d08f7b4b6a5f5ae835ffaf837b676cdb0a6
Eslam Mohamed <eslamm7amed123@gmail.com>
2020-01-23 11:49:15 +0200

    Add Navbar And Home page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container pt-5">
+    <div class="row justify-content-center pt-5">
+        <div class="col-md-8 pt-5">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
