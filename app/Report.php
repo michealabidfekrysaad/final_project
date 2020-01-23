f31dcad1a6349addc09ebc9a9e34966e6f6bec19
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2020-01-23 19:20:42 +0200

    adding viewpoints

--- app/Report.php
+++ app/Report.php
@@ -3,13 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use Laravel\Scout\Searchable;
+
+
 
 class Report extends Model
 {
+    use Searchable;
     protected $fillable = ['name', 'age', 'gender', 'image', 'type', 'special_mark','eye_color','hair_color','city','region','location','last_seen_on','last_seen_at','lost_since','found_since','height','weight',];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function searchableAs()
+    {
+        return 'reports';
     }
 }
