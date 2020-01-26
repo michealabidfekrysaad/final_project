1e02c25ca79b61358c92ca2fafa8e06e71eb98ec
Eslam Mohamed <eslamm7amed123@gmail.com>
2020-01-26 17:34:48 +0200

    Update web

--- routes/web.php
+++ routes/web.php
@@ -51,15 +51,11 @@ Route::get('/users/{id}',function(){
 Route::get('/items/search', function(){
     return view('items.find');
 });
-Route::get('/myprofile', function(){
-    return view('user.index');
-});
+ 
 Route::get('/matchReport', function(){
     return view('matchReport');
 });
-Route::get('/editReport', function(){
-    return view('editReport');
-});
+
 // Route::get('/login/{provider}', 'Auth\LoginController@redirect');
 // Route::get('/login/{provider}/callback', 'Auth\LoginController@callback');
 
