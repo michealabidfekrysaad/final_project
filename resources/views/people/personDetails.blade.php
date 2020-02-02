0e06957c8b7cc007f246271892f2d3bb82886ca9
Islam44 <islamemam95@yahoo.com>
2020-02-02 13:43:11 +0200

    fix personDetails blade

--- resources/views/people/personDetails.blade.php
+++ resources/views/people/personDetails.blade.php
@@ -68,19 +68,18 @@
                       <h3>Hair Color :  </h3>
                       <p> {{$report->hair_color}}</p>
               </div>
-              <div class="fb-share-button" data-href="http://127.0.0.1:8000/showRepo/{{$repor->id}}.com" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F127.0.0.1%3A8000%2FshowRepo%2F2.com&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
-              
-                <!-- <form action="/sendEmail/{{$repor->id}}" method="POST">
-                @csrf
                 <div class="row">
-                      <h3>Description:</h3>
-                      <textarea name="description" cols="10" rows="5"></textarea>
+                    <h3>Write Description to Send :</h3>
                 </div>
-                <br>
-                
-                </form> -->
+                <div class="row">
+                    <form action="/sendEmail/{{$report->id}}" method="POST">
+                        @csrf
+                        <textarea name="description" cols="10" rows="5"></textarea>
+                    </form>
+                </div>
               <div class="row ">
                       <a href="/acceptOtherReport/{{$report->id}}" type="submit" class="btn" id="lostButton">Contact With Report Owner</a>
+                  <div class="fb-share-button" data-href="#" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F127.0.0.1%3A8000%2FshowRepo%2F2.com&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
               </div>
 
 
